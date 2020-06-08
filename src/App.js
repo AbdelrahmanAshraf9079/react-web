83e012cfe418f24b7e9dfa7af42898314fa78502
Abdelrahman Ashraf <47361316+AbdelrahmanAshraf9079@users.noreply.github.com>
2020-06-08 04:58:20 +0200

    "commit"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
 import LoginPage from "./Page1/LoginPage";
-import MainPage from "./Page2/MainPage"
+import MainPage  from './Page2/MainPage';
+import HistoryPage from'./Page3/HistoryPage';
+import StateProvider from './StateProvider';
+import { Switch, Route} from 'react-router';
+import {BrowserRouter} from "react-browser-router" ;
 
 function App() {
-  return (
 
-    //<LoginPage/>
-    <MainPage />
 
+
+return (
+<BrowserRouter>
+
+<StateProvider>
+
+<Switch>
+<Route exact path='/' component={LoginPage} />
+<Route exact path='/MainPage' component={MainPage} />
+<Route exact path='/History' component={HistoryPage} />
+</Switch>
+
+</StateProvider>
+
+</BrowserRouter>
   );
 }
 
